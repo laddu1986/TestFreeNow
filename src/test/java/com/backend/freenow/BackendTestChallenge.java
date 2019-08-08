28b7630232e76eb9ae600446606a9fa20d0847b8
Nitin Bansal <bansalnitin92@gmail.com>
2019-08-08 09:59:39 +0530

    formatting change

--- src/test/java/com/backend/freenow/BackendTestChallenge.java
+++ src/test/java/com/backend/freenow/BackendTestChallenge.java
@@ -21,70 +21,74 @@ public class BackendTestChallenge extends BaseApiTest {
 	private static String USERNAME = "Samantha";
 	private UserSearchResponseDto user;
 	private List<UserPostResponseDto> postList;
-	Map<String,String> param;
-	String URI; 	
+	Map<String, String> param;
+	String URI;
 	String PARAM_USER_ID = "userId";
 	String PARAM_POST_ID = "postId";
-	
+
 	@BeforeClass
 	public void setUp() {
 		RestAssured.baseURI = "https://jsonplaceholder.typicode.com/";
 	}
-	
+
 	// Find the specified user in all users.
 	@Test(priority = 1)
 	public void searchTheUser() {
-	  URI  =  "users";
-	  List<UserSearchResponseDto> userList = requestGET(URI,null, UserSearchResponseDto.class);
-	  
-	  try {
-	  user = userList.stream().filter(k ->k.getUsername().equals(USERNAME)).findFirst().get();
-	  }catch(NoSuchElementException e) {
-		  LOGGER.info(String.format("Username %s is not present in Http response", USERNAME));
-	  }
-	  LOGGER.info(String.format("Username %s has ID %d", USERNAME,user.getId()));	  
+		URI = "users";
+		List<UserSearchResponseDto> userList = requestGET(URI, null, UserSearchResponseDto.class);
+
+		try {
+			user = userList.stream().filter(k -> k.getUsername().equals(USERNAME)).findFirst().get();
+		} catch (NoSuchElementException e) {
+			LOGGER.info(String.format("Username %s is not present in Http response", USERNAME));
+		}
+		LOGGER.info(String.format("Username %s has ID %d", USERNAME, user.getId()));
 	}
-	
-	//Find posts written by specified user. calling SearchTheUser() method again to make the test isolate
-	// Although if above test getting failed, then no use of run below test cases as these are dependent on above.
+
+	// Find posts written by specified user. calling SearchTheUser() method again to
+	// make the test isolate
+	// Although if above test getting failed, then no use of run below test cases as
+	// these are dependent on above.
 	@Test(priority = 2)
 	public void searchPosts() {
-		//searchTheUser();
+		searchTheUser();
 		URI = "posts";
-	    param = new HashMap<String,String>();
-		param.put(PARAM_USER_ID,String.valueOf(user.getId()));
-		
-	    postList = requestGET(URI,param,UserPostResponseDto.class);
-		if(postList!=null && CollectionUtils.hasElements(postList)) {
-			LOGGER.info(String.format("Username %s has posts %d", USERNAME,postList.size()));
-		}else {
+		param = new HashMap<String, String>();
+		param.put(PARAM_USER_ID, String.valueOf(user.getId()));
+
+		postList = requestGET(URI, param, UserPostResponseDto.class);
+		if (postList != null && CollectionUtils.hasElements(postList)) {
+			LOGGER.info(String.format("Username %s has posts %d", USERNAME, postList.size()));
+		} else {
 			LOGGER.warning(String.format("Username %s doesn't have any posts", USERNAME));
 		}
 	}
-	
-	//Find comments for individual posts and validate the email syntax.
-	//making the test independent
+
+	// Find comments for individual posts and validate the email syntax.
+	// making the test independent
 	@Test(priority = 3)
 	public void fetchCommentAndvalidateEmail() {
-//		searchTheUser();
-//		searchPosts();
+		searchTheUser();
+		searchPosts();
 		URI = "comments";
-		param = new HashMap<String,String>();
-		postList.forEach(k->{		
-			
-			param.put(PARAM_POST_ID,String.valueOf(k.getId()));
-			List<PostCommentResponseDto> commentList = requestGET(URI,param,PostCommentResponseDto.class);
-			
-			if(commentList!=null && CollectionUtils.hasElements(commentList)) {
-				List<String> emailList = commentList.stream().map(PostCommentResponseDto::getEmail).collect(Collectors.toList());
+		param = new HashMap<String, String>();
+		postList.forEach(k -> {
+
+			param.put(PARAM_POST_ID, String.valueOf(k.getId()));
+			List<PostCommentResponseDto> commentList = requestGET(URI, param, PostCommentResponseDto.class);
+
+			if (commentList != null && CollectionUtils.hasElements(commentList)) {
+				List<String> emailList = commentList.stream().map(PostCommentResponseDto::getEmail)
+						.collect(Collectors.toList());
 				validateEmailPattern(emailList);
-				LOGGER.info(String.format("PostID %s with comments size %d has valid emails", k.getId(),commentList.size()));
-			}else {
+				LOGGER.info(String.format("PostID %s with comments size %d has valid emails", k.getId(),
+						commentList.size()));
+			} else {
 				LOGGER.warning(String.format("PostID %s doesn't have any comments", k.getId()));
 			}
-			
+
 		});
-		
+
 	}
 
 }
--- src/test/java/com/backend/freenow/BaseApiTest.java
+++ src/test/java/com/backend/freenow/BaseApiTest.java
@@ -17,60 +17,55 @@ import io.restassured.path.json.JsonPath;
 import io.restassured.response.Response;
 
 public class BaseApiTest {
-	
+
 	@SuppressWarnings("deprecation")
 	public BaseApiTest() {
-		RestAssured.config = RestAssuredConfig.config()
-		        .httpClient(HttpClientConfig.httpClientConfig()
-		                .setParam(CoreConnectionPNames.CONNECTION_TIMEOUT, 1000000)
-		                .setParam(CoreConnectionPNames.SO_TIMEOUT, 100000));
+		RestAssured.config = RestAssuredConfig.config().httpClient(
+				HttpClientConfig.httpClientConfig().setParam(CoreConnectionPNames.CONNECTION_TIMEOUT, 1000000)
+						.setParam(CoreConnectionPNames.SO_TIMEOUT, 100000));
 	}
-	
+
 	Response httpresponse;
-	
-	protected final static Logger LOGGER =  
-            Logger.getLogger(Logger.GLOBAL_LOGGER_NAME);
-	
-	public <T> List<T> requestGET(String URI,Map<String,String> params,Class clazz) {
-		if(params!=null) {
-		   httpresponse = RestAssured.given().queryParams(params).get(URI);
-		}else {
-		   httpresponse = RestAssured.given().get(URI);
+
+	protected final static Logger LOGGER = Logger.getLogger(Logger.GLOBAL_LOGGER_NAME);
+
+	public <T> List<T> requestGET(String URI, Map<String, String> params, Class clazz) {
+		if (params != null) {
+			httpresponse = RestAssured.given().queryParams(params).get(URI);
+		} else {
+			httpresponse = RestAssured.given().get(URI);
 		}
-		validateHttpResponse(httpresponse.statusCode(),httpresponse.getContentType());
-		
-		JsonPath path  =  httpresponse.getBody().jsonPath();
+		validateHttpResponse(httpresponse.statusCode(), httpresponse.getContentType());
+
+		JsonPath path = httpresponse.getBody().jsonPath();
 		List<T> responseDto = path.getList("$", clazz);
 		return responseDto;
 	}
-	
-	private void validateHttpResponse(int statusCode,String contentType) {
+
+	private void validateHttpResponse(int statusCode, String contentType) {
 		SkipException exception = new SkipException("Skipping Test...");
-		if(statusCode == 400 || statusCode == 404 ) {
+		if (statusCode == 400 || statusCode == 404) {
 			LOGGER.warning("please check your http request and Auth status");
 			throw exception;
-		}
-		else if(statusCode == 500){
+		} else if (statusCode == 500) {
 			LOGGER.warning("Something went wrog with Website");
 			throw exception;
-		}
-		else if ((statusCode == 200 || statusCode == 201) && contentType.contains("json") ) {
+		} else if ((statusCode == 200 || statusCode == 201) && contentType.contains("json")) {
 			LOGGER.info("everything went well. Cheers!!");
-		}
-		else {
+		} else {
 			LOGGER.warning("Application seems to be down at this moment");
 			throw exception;
 		}
 	}
-	
+
 	protected void validateEmailPattern(List<String> email) {
-	 Pattern VALID_EMAIL_ADDRESS_REGEX = 
-			    Pattern.compile("^[A-Z0-9._%+-]+@[A-Z0-9.-]+\\.[A-Z]{2,6}$", Pattern.CASE_INSENSITIVE);
-	 
-	email.forEach(k->{
-		Matcher matcher = VALID_EMAIL_ADDRESS_REGEX.matcher(k);
-		Assert.assertTrue(matcher.find());
-	});
+		Pattern VALID_EMAIL_ADDRESS_REGEX = Pattern.compile("^[A-Z0-9._%+-]+@[A-Z0-9.-]+\\.[A-Z]{2,6}$",
+				Pattern.CASE_INSENSITIVE);
+
+		email.forEach(k -> {
+			Matcher matcher = VALID_EMAIL_ADDRESS_REGEX.matcher(k);
+			Assert.assertTrue(matcher.find());
+		});
 	}
 
 }
